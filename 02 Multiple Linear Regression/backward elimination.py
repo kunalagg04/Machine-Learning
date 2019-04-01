2ba048334a0d62b8f12a6a2853ee069bbde4ce51
KunaL AggarwaL <kunalagg04@gmail.com>
2019-04-01 16:52:58 +0530

    Update backward elimination.py

--- 02 Multiple Linear Regression/backward elimination.py
+++ 02 Multiple Linear Regression/backward elimination.py
@@ -18,8 +18,6 @@ X = onehotencoder.fit_transform(X).toarray()
 #removing first column 
 X = X[:, 1:]
 
-X_train , X_test , Y_train , Y_test = train_test_split(X,Y , test_size=0.2 , random_state = 0)
-
 #building model with backward elimination
 import statsmodels.formula.api as sm
 #adding x0 column to X matri
